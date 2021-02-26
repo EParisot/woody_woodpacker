ce325165a268ea22bed5c15cfabf0bf1b80a2928
eparisot <eparisot@student.42.fr>
2021-02-26 18:20:02 +0100

    copy elf OK

--- srcs/woody_woodpacker.c
+++ srcs/woody_woodpacker.c
@@ -12,23 +12,38 @@
 
 #include "../includes/woody_woodpacker.h"
 
-static void handle_obj(void *obj)
+static int dump_obj(void *obj, size_t size)
 {
-	Elf64_Ehdr	*ehdr = (Elf64_Ehdr *)obj;
-	Elf64_Shdr 	*shdr = (Elf64_Shdr *)(obj + ehdr->e_shoff);
-	Elf64_Shdr 	*sh_strtab = &shdr[ehdr->e_shstrndx];
-	const char *sh_strtab_p = obj + sh_strtab->sh_offset;
-	int shnum = ehdr->e_shnum;
+	int fd;
 
+	if ((fd = open("woody", O_WRONLY)) < 0)
+	{
+		printf("Error creating 'woody' file.\n");
+		return (-1);
+	}
+	write(fd, obj, size);
+	close(fd);
+	return (0);
+}
+
+static void handle_obj(void *obj, size_t size)
+{
+	Elf64_Ehdr *ehdr = (Elf64_Ehdr *)obj;
+	int shnum = ehdr->e_shnum;
+	Elf64_Shdr *shdr = (Elf64_Shdr *)(obj + ehdr->e_shoff);
+	Elf64_Shdr *sh_strtab = &shdr[ehdr->e_shstrndx];
+	const char *sh_strtab_p = obj + sh_strtab->sh_offset;
+	
 	for (int i = 0; i < shnum; ++i) {
     	printf("%2d: %4d '%s'\n", i, shdr[i].sh_name, sh_strtab_p + shdr[i].sh_name);
   	}
+	// TODO corrupt file
+	dump_obj(obj, size);
 }
 
-static void 		woody_woodpacker(void *obj, void *end, char *obj_name)
+static void 		woody_woodpacker(void *obj, size_t size, char *obj_name)
 {
 	char	magic_nb[5];
-	(void)end;
 
 	ft_strncpy(magic_nb, obj, 5);
 	if (magic_nb[0] == 0x7f && \
@@ -38,7 +53,7 @@ static void 		woody_woodpacker(void *obj, void *end, char *obj_name)
 		magic_nb[4] == ELFCLASS64)
 	{
 		printf("ELF64 detected in %s\n", obj_name);
-		handle_obj(obj);
+		handle_obj(obj, size);
 	}
 	else
 	{
@@ -50,27 +65,27 @@ static void			read_obj(char *obj_name)
 {
 	int				fd;
 	void			*obj;
-	struct stat		buf;
+	size_t			size;
 
 	if ((fd = open(obj_name, O_RDONLY)) < 0)
 	{
 		print_err("Error openning file", obj_name);
 		return ;
 	}
-	if (fstat(fd, &buf) < 0)
+	if ((size = lseek(fd, (size_t)0, SEEK_END)) <= 0)
 	{
-		print_err("Error stating file", obj_name);
+		print_err("Error empty file", obj_name);
 		close(fd);
 		return ;
 	}
-	if ((obj = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == \
+	if ((obj = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0)) == \
 			MAP_FAILED)
 		print_err("Error mapping file", obj_name);
 	else
 	{
 		close(fd);
-		woody_woodpacker(obj, obj + buf.st_size, obj_name);
-		if (munmap(obj, buf.st_size) < 0)
+		woody_woodpacker(obj, size, obj_name);
+		if (munmap(obj, size) < 0)
 			print_err("Error munmap", "");
 	}
 }
@@ -82,10 +97,7 @@ int main(int argc, char **argv)
 	else if (argc == 1)
 		read_obj("a.out\0");
 	else {
-		for (int i = 1; i < argc; ++i)
-		{
-			read_obj(argv[i]);
-		}
+		printf("Error: Too many args...\n");
 	}
 	return (0);
 }
