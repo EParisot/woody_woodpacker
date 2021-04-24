8b41c0cdaadd1e3de595e6d4406365e2395509da
eparisot <eparisot@student.42.fr>
2021-04-24 19:30:07 +0200

    refactor print

--- srcs/woody_woodpacker.c
+++ srcs/woody_woodpacker.c
@@ -26,49 +26,68 @@ static int dump_obj(void *obj, size_t size)
 	return (0);
 }
 
-static void handle_obj(void *obj, size_t size)
+static void debug_dump(unsigned int *data, size_t start_addr, size_t size)
 {
-	void *obj_cpy;
+	for (size_t j = 0; j * 4 < size; j += 1)
+	{
+		if (j % 4 == 0)
+			printf("\n %04lx - ", start_addr + j * 4);
+		printf("%08x ", cpu_32(data[j]));
+	}
+	printf("\n");
+}
+
+unsigned int *get_text(void *obj, size_t *text_size) 
+{
 	unsigned int *text_content;
-	size_t text_size;
 	Elf64_Ehdr *ehdr = (Elf64_Ehdr *)obj;
 	int shnum = ehdr->e_shnum;
 	Elf64_Shdr *shdr = (Elf64_Shdr *)(obj + ehdr->e_shoff);
 	Elf64_Shdr *sh_strtab = &shdr[ehdr->e_shstrndx];
 	const char *sh_strtab_p = obj + sh_strtab->sh_offset;
-	
-	for (int i = 0; i < shnum; ++i) {
+
+	for (int i = 0; i < shnum; ++i)
+	{
+		// get .text section
 		if (ft_strequ(sh_strtab_p + shdr[i].sh_name, ".text"))
 		{
-			text_size = shdr[i].sh_size;
-			// align size on 8
-			while (text_size % 8 != 0)
-				text_size++;
-			printf("text size = %ld\n", text_size);
-			if ((text_content = malloc(text_size)) == NULL)
-			{
-				printf("Error copying text section.\n");
-				return ;
-			}
-			ft_bzero(text_content, text_size);
-			ft_memcpy(text_content, obj + shdr[i].sh_offset, text_size);
-			for (size_t j = 0; j * 4 < text_size; j += 1)
-			{
-				if (j % 4 == 0)
-					printf("\n %04lx - ", j);
-				printf("%08x ", cpu_32(text_content[j]));
-			}
-			printf("\n");
-			free(text_content);
+			*text_size = shdr[i].sh_size;
+			// align sh size on 8
+			while (*text_size % 8 != 0)
+				(*text_size)++;
+			printf("text size = %ld\n", *text_size);
+			if ((text_content = malloc(*text_size)) == NULL)
+				return NULL;
+			ft_bzero(text_content, *text_size);
+			ft_memcpy(text_content, obj + shdr[i].sh_offset, *text_size);
+			debug_dump(text_content, shdr[i].sh_offset, *text_size);
+			return(text_content);
 		}
   	}
+	return NULL;
+}
+
+static void handle_obj(void *obj, size_t size)
+{
+	void *obj_cpy;
+	unsigned int *text_content;
+	size_t text_size;
+
+	// get text content
+	if ((text_content = get_text(obj, &text_size)) == NULL)
+	{
+		printf("Error: copying text section.\n");
+		return ;
+	}
+	free(text_content);
+	// copy origin file
 	if ((obj_cpy = malloc(size)) == NULL)
 	{
-		printf("Error: can't duplicate file...");
+		printf("Error: can't duplicate file.\n");
 		return ;
 	}
 	ft_memcpy(obj_cpy, obj, size);
-	// TODO corrupt file
+	// TODO encrypt file
 	dump_obj(obj_cpy, size);
 	free(obj_cpy);
 }
