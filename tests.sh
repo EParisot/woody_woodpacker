909072aa797ae89896cb1ca30b39d5e32a714c24
eparisot <eparisot@student.42.fr>
2021-05-22 18:10:15 +0200

    improve tests script

--- tests.sh
+++ tests.sh
@@ -4,6 +4,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 make re
@@ -12,6 +13,7 @@ printf "\n${RED}* Tests Classic: ${NC}\n"
 printf "\n${YELLOW}* Sample32 ${NC}\n"
 ./woody_woodpacker ressources/sample32
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
@@ -19,6 +21,7 @@ fi
 printf "\n${YELLOW}* Sample64 NO PIE ${NC}\n"
 ./woody_woodpacker ressources/sample64_no_pie
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
@@ -26,6 +29,7 @@ fi
 printf "\n${YELLOW}* Sample64 PIE ${NC}\n"
 ./woody_woodpacker ressources/sample64
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
@@ -33,6 +37,7 @@ fi
 printf "\n${YELLOW}* Sample64 STRIPPED ${NC}\n"
 ./woody_woodpacker ressources/sample64_stripped
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
@@ -40,6 +45,7 @@ fi
 printf "\n${YELLOW}* Sample64 with ARGS NO PIE ${NC}\n"
 ./woody_woodpacker ressources/sample_args_no_pie
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody test
 	rm -f woody
 fi
@@ -47,6 +53,7 @@ fi
 printf "\n${YELLOW}* Sample64 with ARGS PIE ${NC}\n"
 ./woody_woodpacker ressources/sample_args
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody test
 	rm -f woody
 fi
@@ -54,29 +61,33 @@ fi
 printf "\n${YELLOW}* Sample64 with ARGS STRIPPED ${NC}\n"
 ./woody_woodpacker ressources/sample_args_stripped
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody test
 	rm -f woody
 fi
 
-printf "\n${YELLOW}* /usr/bin/date ${NC}\n"
+printf "\n${YELLOW}* ./woody_woodpacker /usr/bin/date ${NC}\n"
 ./woody_woodpacker /usr/bin/date
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
 
-printf "\n${YELLOW}* /usr/bin/ls ${NC}\n"
+printf "\n${YELLOW}* ./woody_woodpacker /usr/bin/ls ${NC}\n"
 ./woody_woodpacker /usr/bin/ls
 if [ -f "woody" ]; then
+	printf "\n${BLUE}./woody [args]: ${NC}\n"
 	./woody
 	rm -f woody
 fi
 
 printf "\n${RED}* Tests Corrupted: ${NC}\n"
 for file in "corrupted"/*; do
-	printf "\n${YELLOW}* $file ${NC}\n"
+	printf "\n${YELLOW}* ./woody_woodpacker $file: ${NC}\n"
 	./woody_woodpacker $file
 	if [ -f "woody" ]; then
+		printf "\n${BLUE}./woody [args]: ${NC}\n"
 		./woody
 		rm -f woody
 	fi
